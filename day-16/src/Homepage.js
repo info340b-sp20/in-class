1449141b8be7f9d39f9c9e8a732a01295b7ce73e
Jacob A. Morris <jacob.morris@gmail.com>
2020-05-21 08:57:55 -0700

    day 16 demo

---
+++ day-16/src/Homepage.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import { Jumbotron } from 'reactstrap';
+
+export class Homepage extends Component {
+    render() {
+        return (
+            <Jumbotron>
+                <h1>Jacob A. Morris</h1>
+            </Jumbotron>
+        );
+    }
+}
